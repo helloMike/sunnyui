8748709e7896b21859e40bd924d685be5be4f987
YXZONE <xiaoxing200888@gmail.com>
2016-01-29 02:01:02 +0800

    20160129 2:00 sunnyui首页 动画效果顶部导航栏

--- js/main.js
+++ js/main.js
@@ -3,4 +3,31 @@
  */
 $(document).ready(function(){
     $('#myCarousel').carousel('cycle');
+    //
+    $(window).bind('scroll',function(){
+        var window_height = $(window).height();
+        var window_width = $(window).width();
+        var scrolltop_height = window_height * 0.3;
+        if(window_width > 768){
+            if($(window).scrollTop() > scrolltop_height){
+                $('#top_nav').addClass('on');
+            }else{
+                $('#top_nav').removeClass('on');
+            }
+        }
+    })
+    //
+    $(window).bind('resize',function(){
+        var window_width = $(window).width();
+        var window_height = $(window).height();
+        var scrolltop_height = window_height * 0.3;
+        var screen_width = 768;
+        if(window_width <= screen_width){
+            $('#top_nav').addClass('on');
+        }else if($(window).scrollTop() < scrolltop_height){
+            $('#top_nav').removeClass('on');
+        }
+    })
+
+
 });
